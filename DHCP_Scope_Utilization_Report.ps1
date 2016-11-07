cb12ae1cbd6bcddf1657c12df4bd7a3e8e1baf10
avacabro <spcloud@gmail.com>
2016-11-07 08:19:04 -0800

    Add files via upload

---
+++ DHCP_Scope_Utilization_Report.ps1
@@ -0,0 +1,13 @@
+﻿$threshold = 75
+$email_to = "scloud@sbch.org"
+$email_from = "scloud@sbch.org"
+$email_server = "smtprelay.sbch.org"
+$date = Get-Date
+$report = "C:\temp\DHCP\report.csv"
+
+
+Get-DhcpServerv4ScopeStatistics -computername chs-dhcp2.sbch.org | Select ScopeID, PercentageInUse, Free, InUse | Where PercentageInUse -gt $threshold | Export-Csv $report -NoTypeInformation
+
+Send-MailMessage -To $email_to -From $email_from -SmtpServer $email_server -Subject "DHCP Scope Report - $date" -Attachments $report
+
+Remove-Item $report
